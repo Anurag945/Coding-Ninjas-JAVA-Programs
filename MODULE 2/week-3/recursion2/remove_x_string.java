e36d3d1c9f0aa9c9b7105c0dc1670897ecb5abbc
anurag945 <anuragsinha945@gmail.com>
2024-07-26 16:53:22 +0530

    file

---
+++ MODULE 2/week-3/recursion2/remove_x_string.java
@@ -0,0 +1,23 @@
+
+
+public class remove_x_string {
+
+    public static String removex(String str){
+        if(str.length()==0){
+            return str;
+        }
+
+        String ans= "";
+        if(str.charAt(0)!='x'){
+            ans=ans+str.charAt(0);
+        }
+
+        String smallans = removex(str.substring(1));
+        return ans+smallans;
+    }   
+    
+    public static void main(String[] args) {
+        String name="xxaxnxuxxrxxaxxg";
+        System.out.println(removex(name));
+    }
+}
